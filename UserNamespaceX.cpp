ab889beceea65bf37f7a646a50f558a80e92f020
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-04-07 01:12:15 +0530

    Add files via upload

---
+++ UserNamespaceX.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+namespace PPA
+{
+    int i = 11;
+     
+}
+namespace LB
+{
+    int j = 21;
+} 
+using namespace LB;
+int main()
+
+{
+    cout<<PPA::i<<"\n";  //11
+    cout<<LB::j<<"\n";      //21
+  return 0;
+}
